2ea969f66ae3894c3790e52a375aec42eeacbb0e
SHOOTDEAD <117431495+SHOOTDEAD@users.noreply.github.com>
2023-04-22 10:00:05 +0530

    Update get_logs.py

--- validifi/get_logs.py
+++ validifi/get_logs.py
@@ -1,1 +1,18 @@
-#will be updated soon
+import pandas as pd
+class get_logs:
+    def __init__(self):
+        pass
+       
+    def convert_logs(self,data):
+        data = list(zip(*[list(i) for i in data]))
+        logs = pd.DataFrame({
+        'Filename':data[0],
+        'status':data[1]
+        }).to_csv(index=False)
+        
+        return logs.encode()
+    def func(self,cursor):
+        log_data = cursor.execute('select * from logs')
+        log_data = log_data.fetchall()
+        file = self.convert_logs(log_data)
+        return file
