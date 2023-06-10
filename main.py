20f4cc6a1da6919c5155bb46898f43b819ec1898
SHOOTDEAD <117431495+SHOOTDEAD@users.noreply.github.com>
2023-06-10 12:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,50 @@ import validifi
 import duckdb
  
 app=FastAPI()
-
+db_file_name = 'logs.db'
+db_query = "insert into logs values(?,?)"
 @app.post('/validate_Configuration_1')
-async def Validate_Configuration_1(file : UploadFile=File(...)):
+async def validate_configuration_1(file : UploadFile=File(...)):
     b_file= await file.read()
     
     validation=validifi.c1.verify(file.filename,b_file).func()
-    db=duckdb.connect('logs.db')
+    db=duckdb.connect(db_file_name)
     cursor=db.cursor()
     if type(validation) == bytes:
-        cursor.execute("insert into logs values(?,?)",(file.filename,1))
+        cursor.execute(db_query,(file.filename,1))
         cursor.commit()
         cursor.close()
         return {'status':1,'file':validation}
-    cursor.execute("insert into logs values(?,?)",(file.filename,validation))
+    cursor.execute(db_query,(file.filename,validation))
     db.commit()
     db.close()
     return {'status':0,'error':validation}
 @app.post('/validate_Configuration_2')
-async def Validate_Configuration_2(file : UploadFile=File(...)):
+async def validate_configuration_2(file : UploadFile=File(...)):
     b_file= await file.read()
     
     validation=validifi.c2.verify(file.filename,b_file).func()
-    db=duckdb.connect('logs.db')
+    db=duckdb.connect(db_file_name')
     cursor=db.cursor()
     if type(validation) == bytes:
-        cursor.execute("insert into logs values(?,?)",(file.filename,1))
+        cursor.execute(db_query,(file.filename,1))
         cursor.commit()
         cursor.close()
         return {'status':1,'file':validation}
-    cursor.execute("insert into logs values(?,?)",(file.filename,validation))
+    cursor.execute(db_query,(file.filename,validation))
     db.commit()
     db.close()
     return {'status':0,'error':validation}
 @app.post('/get_Configuration')
-async def Get_Configuration(file : UploadFile=File(...)):
+# code yet to update
+async def get_configuration(file : UploadFile=File(...)):
     pass
 @app.get('/get_Logs')
-async def Get_Logs():
-    db = duckdb.connect('logs.db')
+async def get_logs():
+    db = duckdb.connect(db_file_name)
     cursor = db.cursor()
-    File = validifi.get_logs.get_logs().func(cursor)
-    return {'file':File}
+    files = validifi.get_logs.get_logs().func(cursor)
+    return {'file':files}
 
    
 
