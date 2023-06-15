4d00b4eb268f3b15307a2990d36df00e875030b8
LOKESH-999 <104671058+LOKESH-999@users.noreply.github.com>
2023-06-15 13:27:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,33 @@ import validifi
 import duckdb
  
 app=FastAPI()
-db_file_name = 'logs.db'
-db_query = "insert into logs values(?,?)"
+
+def update(file_name,validation):
+    db=duckdb.connect('logs.db')
+    cursor=db.cursor()
+    ret_val={'status':0,'file':validation}
+    if type(validation)==bytes:
+        cursor.execute("insert into logs values(?,?)",(file_name,1))
+        ret_val['status']=1
+    else :
+        cursor.execute("insert into logs values(?,?)",(file_name,validation))
+    cursor.commit()
+    cursor.close()
+    return ret_val
+ 
 @app.post('/validate_Configuration_1')
 async def validate_configuration_1(file : UploadFile=File(...)):
     b_file= await file.read()
-    
     validation=validifi.configuration1.verify(file.filename,b_file).func()
-    db=duckdb.connect(db_file_name)
-    cursor=db.cursor()
-    if type(validation) == bytes:
-        cursor.execute(db_query,(file.filename,1))
-        cursor.commit()
-        cursor.close()
-        return {'status':1,'file':validation}
-    cursor.execute(db_query,(file.filename,validation))
-    db.commit()
-    db.close()
-    return {'status':0,'error':validation}
+    return update(file.filename,validation)
+ 
 @app.post('/validate_Configuration_2')
 async def validate_configuration_2(file : UploadFile=File(...)):
     b_file= await file.read()
     
     validation=validifi.configuration2.verify(file.filename,b_file).func()
-    db=duckdb.connect(db_file_name)
-    cursor=db.cursor()
-    if type(validation) == bytes:
-        cursor.execute(db_query,(file.filename,1))
-        cursor.commit()
-        cursor.close()
-        return {'status':1,'file':validation}
-    cursor.execute(db_query,(file.filename,validation))
-    db.commit()
-    db.close()
-    return {'status':0,'error':validation}
+    return update(file.filename,validation)
+ 
 @app.post('/get_Configuration')
 # code yet to update
 async def get_configuration(file : UploadFile=File(...)):
